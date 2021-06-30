766443431877c2b21fa76b0e6aadf03afff00c73
nagehancapa <capanagehan@gmail.com>
2021-06-30 19:38:24 +0200

    setup devDependencies and dist folder

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
     // })]),
   ],
   output: {
-    path: path.join(__dirname, "public"),
+    path: path.join(__dirname, "public", "dist"),
     filename: "bundle.js",
     clean: true,
   },
